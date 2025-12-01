27f4ccecf632523f100189ff3d5f64d31299e77b
federus1105 <federusrudi@gmail.com>
2025-12-01 23:39:33 +0700

    Feat ( layouts ) : feature main layout for pages main

--- src/layouts/MainLayouts.jsx
+++ src/layouts/MainLayouts.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { ScrollToTop } from "../route/Routes";
+import Navbar from "../components/navbar/Navbar";
 
 function MainLayouts() {
   return (
     <>
-      
+      <ScrollToTop />
+      <Navbar />
+      <Outlet />
     </>
-  )
+  );
 }
 
-export default MainLayouts
+export default MainLayouts;
