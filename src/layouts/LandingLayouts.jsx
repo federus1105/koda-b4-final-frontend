6fa2f4e1791c4706427c9f179bc7ecb639f9e95b
federus1105 <federusrudi@gmail.com>
2025-12-03 01:18:03 +0700

    Feat ( layouts ) : feature logic navbar for state auth and using loading components

--- src/layouts/LandingLayouts.jsx
+++ src/layouts/LandingLayouts.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { ScrollToTop } from "../route/Routes";
 import { Outlet } from "react-router-dom";
 import NavbarLanding from "../components/navbar/NavbarLanding";
+import { useSelector } from "react-redux";
+import Navbar from "../components/navbar/Navbar";
 
 function LandingLayouts() {
+  const [loading, setLoading] = useState(false);
+  const { isLoggedIn } = useSelector((state) => state.auth);
   return (
     <>
       <ScrollToTop />
-      <NavbarLanding />
-      <Outlet />
+      {isLoggedIn ? <Navbar /> : <NavbarLanding />}
+      {loading && (
+        <div className="loader-overlay">
+          <div className="flex flex-col items-center">
+            <div className="loader"></div>
+            <span className="loader-text">Loading...</span>
+          </div>
+        </div>
+      )}
+      <Outlet context={{ setLoading }} />
     </>
   );
 }
