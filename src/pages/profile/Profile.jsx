bcf4424c75e893006706dc6fefdaa1f3ec4909ea
federus1105 <federusrudi@gmail.com>
2025-12-03 01:46:09 +0700

    Feat ( pages ) : feature get data profile from redux state

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { User, Lock, Upload, Save, X } from "lucide-react";
 import placeholder from "/placeholderProfile.webp";
+import { useSelector } from "react-redux";
 
 function Profile() {
+  const profile = useSelector((state) => state.auth.currentUser);
   const [fileName, setFileName] = useState("");
   const handleDelete = () => {
     setFileName("");
@@ -28,85 +30,94 @@ function Profile() {
               </h2>
             </div>
 
-            {/* Profile Picture */}
-            <div className="mb-6">
-              <label className="block text-sm font-medium text-gray-700 mb-3">
-                Profile Picture
-              </label>
-              <div className="flex items-center gap-4">
-                <img
-                  src={placeholder}
-                  alt="Profile"
-                  className="w-16 h-16 rounded-full object-cover"
-                />
-                <div className="flex flex-col gap-2">
-                  <label className="flex items-center gap-2 px-4 py-2 border border-gray-300 rounded-lg text-gray-700 text-sm font-medium hover:bg-gray-50 cursor-pointer">
-                    <Upload className="w-4 h-4" />
-                    Upload New Photo
-                    <input
-                      type="file"
-                      accept="image/*"
-                      className="hidden"
-                      onChange={(e) => {
-                        const file = e.target.files[0];
-                        if (file) setFileName(file.name);
-                      }}
-                    />
-                  </label>
+            <form>
+              {/* Profile Picture */}
+              <div className="mb-6">
+                <label className="block text-sm font-medium text-gray-700 mb-3">
+                  Profile Picture
+                </label>
+                <div className="flex items-center gap-4">
+                  <img
+                    src={profile.photos || placeholder}
+                    alt="Profile"
+                    className="w-16 h-16 rounded-full object-cover"
+                  />
+                  <div className="flex flex-col gap-2">
+                    <label className="flex items-center gap-2 px-4 py-2 border border-gray-300 rounded-lg text-gray-700 text-sm font-medium hover:bg-gray-50 cursor-pointer">
+                      <Upload className="w-4 h-4" />
+                      Upload New Photo
+                      <input
+                        type="file"
+                        accept="image/*"
+                        className="hidden"
+                        onChange={(e) => {
+                          const file = e.target.files[0];
+                          if (file) setFileName(file.name);
+                        }}
+                      />
+                    </label>
 
-                  {fileName && (
-                    <div className="flex items-center justify-between text-xs text-gray-700 px-3 py-2 bg-gray-100 border border-gray-200 rounded-md">
-                      <span className="truncate max-w-[200px]">{fileName}</span>
+                    {fileName && (
+                      <div className="flex items-center justify-between text-xs text-gray-700 px-3 py-2 bg-gray-100 border border-gray-200 rounded-md">
+                        <span className="truncate max-w-[200px]">
+                          {fileName}
+                        </span>
 
-                      <button
-                        onClick={handleDelete}
-                        className="cursor-pointer ml-2 text-gray-500 hover:text-red-500 transition"
-                      >
-                        <X className="w-4 h-4" />
-                      </button>
-                    </div>
-                  )}
+                        <button
+                          onClick={handleDelete}
+                          className="cursor-pointer ml-2 text-gray-500 hover:text-red-500 transition"
+                        >
+                          <X className="w-4 h-4" />
+                        </button>
+                      </div>
+                    )}
+                  </div>
                 </div>
               </div>
-            </div>
 
-            {/* Full Name */}
-            <div className="mb-6">
-              <label
-                htmlFor="fullname"
-                className="block text-sm font-medium text-gray-700 mb-2"
+              {/* Full Name */}
+              <div className="mb-6">
+                <label
+                  htmlFor="fullname"
+                  className="block text-sm font-medium text-gray-700 mb-2"
+                >
+                  Full Name
+                </label>
+                <input
+                  type="text"
+                  id="fullname"
+                  value={profile.fullname}
+                  placeholder="Enter your Fullname"
+                  className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                />
+              </div>
+
+              {/* Email Address */}
+              <div className="mb-6">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700 mb-2"
+                >
+                  Email Address
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  value={profile.email}
+                  placeholder="Enter your Email"
+                  className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                />
+              </div>
+
+              {/* Save Button */}
+              <button
+                type="submit"
+                className="cursor-pointer flex items-center gap-2 bg-blue-600 text-white px-6 py-3 rounded-lg font-medium hover:bg-blue-700"
               >
-                Full Name
-              </label>
-              <input
-                type="text"
-                id="fullname"
-                placeholder="Enter your Fullname"
-                className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-              />
-            </div>
-
-            {/* Email Address */}
-            <div className="mb-6">
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700 mb-2"
-              >
-                Email Address
-              </label>
-              <input
-                type="email"
-                id="email"
-                placeholder="Enter your Email"
-                className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-              />
-            </div>
-
-            {/* Save Button */}
-            <button className="cursor-pointer flex items-center gap-2 bg-blue-600 text-white px-6 py-3 rounded-lg font-medium hover:bg-blue-700">
-              <Save className="w-4 h-4" />
-              Save Changes
-            </button>
+                <Save className="w-4 h-4" />
+                Save Changes
+              </button>
+            </form>
           </div>
 
           {/* Change Password Section */}
